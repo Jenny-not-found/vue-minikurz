27a1686eb8045d2d6ff3a1c3f02877ba4920eeea
Student Student <student227@primakurzy.cz>
2024-10-11 13:51:50 +0200

    zaklady vue

---
+++ zoo/src/components/PocetNavstevniku.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue";
+const pocet = ref(100);
+
+setInterval(() => {
+    //kdykoliv budeme chtit cist nebo menit hodnotu reaktivni promenne, tak musime jit pres vlastnost value.
+    pocet.value = Math.floor(Math.random() * 10000);
+}, 2000);
+</script>
+
+<template>
+    <!-- double mustache rika ze zde se ma vypast nejaka reaktivni hodnota -->
+    <!-- double mustache nemuzete pouzit v atributu elementu -->
+    <p>Počet návštěvníků: {{ pocet }}</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ zoo/src/components/UkolOdpocitavadlo.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from "vue";
+const cislo = ref(10);
+
+
+const odpocet = setInterval(() => {
+    cislo.value--; 
+    
+    if (cislo.value <= 0) {
+        cislo.value = "HOTOVO!"
+        clearInterval(odpocet);
+    }
+}, 1000);
+
+
+</script>
+
+<template>
+    <h2>Odpočet</h2>
+    <p>{{ cislo }}</p>
+</template>
+
+<style scoped>
+
+p {
+    
+        color:black
+    }
+
+</style>
